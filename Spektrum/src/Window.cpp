bae3b42b9a28f79825ed46baa380c1c3e3db8225
h3rl <halvard.erla@gmail.com>
2023-05-08 09:51:02 +0200

    create custom window

---
+++ Spektrum/src/Window.cpp
@@ -0,0 +1,76 @@
+#include "Window.h"
+
+
+Window::Window() :
+m_viewAxis(WindowAxis::Mathematical),
+m_viewOrigin(WindowOrigin::BottomLeft)
+{
+
+}
+
+sf::View& Window::getSizedView()
+{
+	sf::View view = this->getDefaultView();
+	sf::Vector2f wSize = (sf::Vector2f)this->getSize();
+
+	view.setSize(wSize);
+	if (m_viewOrigin == Center)
+	{
+		view.setCenter(0,0);
+	}
+	else
+	{
+		view.setCenter(wSize.x / 2.f, wSize.y / 2.f);
+	}
+
+	return view;
+}
+
+void Window::updateSize()
+{
+
+	sf::View view = this->getDefaultView();
+
+	sf::Vector2u wSize = this->getSize();
+	const float wWidth = wSize.x;
+	const float wHeight = wSize.y;
+
+	// Correct axis
+	switch (this->m_viewAxis)
+	{
+	case Mathematical:
+		view.setSize(wWidth, -wHeight);
+		break;
+	case Computer:
+		view.setSize(wWidth, wHeight);
+		break;
+	default:
+		break;
+	}
+
+	// Correct origin
+	sf::Vector2f center{ wWidth / 2.f,wHeight / 2.f };
+	switch (this->m_viewOrigin)
+	{
+	case Center:
+		center = { 0,0 };
+		break;
+	case TopLeft:
+		center.y *= -1.f;
+		break;
+	case BottomRight:
+		center.x *= -1.f;
+		break;
+	case TopRight:
+		center.x *= -1.f;
+		center.y *= -1.f;
+		break;
+	case BottomLeft:
+		break;
+	default:
+		break;
+	}
+	view.setCenter(center);
+
+	this->setView(view);
+}
---
+++ Spektrum/src/Window.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "stdafx.h"
+
+
+enum WindowOrigin : int
+{
+	Center,
+	TopLeft,
+	TopRight,
+	BottomLeft,
+	BottomRight
+};
+
+enum WindowAxis : int
+{
+	Mathematical, // x left, y up
+	Computer // x right, y down
+};
+
+class Window : public sf::RenderWindow
+{
+public:
+							Window();
+
+	sf::View&				getSizedView();
+	void					updateSize();
+private:
+	WindowOrigin			m_viewOrigin;
+	WindowAxis				m_viewAxis;
+};
